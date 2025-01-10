3f36dff323f2aae64100b58b23e60e9b02d5c577
kidow <wcgo2ling@gmail.com>
2025-01-10 23:19:36 +0900

    chore: set prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,23 @@
+const config = {
+  singleQuote: true,
+  semi: false,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'none',
+  printWidth: 80,
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '^components/(.*)$',
+    '^services/(.*)$',
+    '^server/(.*)$',
+    '^[./]'
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss'
+  ]
+}
+
+export default config
